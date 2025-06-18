823d658eb608bfa1e869108ccbb6f531aaf56bfb
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-06-18 21:58:19 +0600

    first commit

---
+++ Basics.py
@@ -0,0 +1,18 @@
+# print('This is a crash course to learn AI and ML')
+
+# age = 23
+# name = 'Bengal Tiger'
+# print('%s Is %d Years Old.' % (name, age))
+
+# name = input('Enter Your Name : ')
+# age = int(input('Enter Your Age : '))
+# print(f'{name} Is {age} Years Old.')
+
+x = [3, 'Hello', 1.2]
+print('x[0] : ', x[0])
+print('x[1] : ', x[1])
+print('x[2] : ', x[2])
+print('x[-1] : ', x[-1])
+print('x[-2] : ', x[-2])
+print(x[:100])
+print(len(x[:100]))
